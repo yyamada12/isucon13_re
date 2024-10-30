c710976795a74eb8706113b8560b23e4b42d170b
yyamada12 <12yacropolisy@gmail.com>
2024-10-30 06:42:30 +0000

    [105814] add index to isudns.records

--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -5,3 +5,5 @@ alter table livestreams add index idx_user_id(user_id);
 alter table reactions add index idx_livestream_id(livestream_id);
 alter table themes add index idx_user_id(user_id);
 alter table ng_words add index idx_user_id_livestream_id(user_id, livestream_id);
+
+alter table records add index idx_name(`name`);
