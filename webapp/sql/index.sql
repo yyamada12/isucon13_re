c1550a6a27e3dc7d6d94cb0f39b3f89ae8787ce5
yyamada12 <12yacropolisy@gmail.com>
2024-10-30 06:49:04 +0000

    [103403] change index for ng_words

--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -4,6 +4,7 @@ alter table livecomments add index idx_livestream_id(livestream_id);
 alter table livestreams add index idx_user_id(user_id);
 alter table reactions add index idx_livestream_id(livestream_id);
 alter table themes add index idx_user_id(user_id);
-alter table ng_words add index idx_user_id_livestream_id(user_id, livestream_id);
+alter table ng_words add index idx_livestream_id_user_id(livestream_id, user_id);
+alter table reservation_slots add index idx_start_at_end_at(start_at, end_at);
 
 alter table records add index idx_name(`name`);
