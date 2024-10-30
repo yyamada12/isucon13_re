a5922bb37438442f1d021cc2927a5068f4204bdd
yyamada12 <12yacropolisy@gmail.com>
2024-10-30 06:38:44 +0000

    [114411] use interpolateParams for admin prepare

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -72,6 +72,7 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	conf.Passwd = "isucon"
 	conf.DBName = "isupipe"
 	conf.ParseTime = true
+	conf.InterpolateParams = true
 
 	if v, ok := os.LookupEnv(networkTypeEnvKey); ok {
 		conf.Net = v
