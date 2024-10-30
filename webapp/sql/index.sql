fe93fcc079d3a56e8e17f304277d1373a03eb6fd
yyamada12 <12yacropolisy@gmail.com>
2024-10-30 06:27:52 +0000

    [102787] add index to ng_words

--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -4,3 +4,4 @@ alter table livecomments add index idx_livestream_id(livestream_id);
 alter table livestreams add index idx_user_id(user_id);
 alter table reactions add index idx_livestream_id(livestream_id);
 alter table themes add index idx_user_id(user_id);
+alter table ng_words add index idx_user_id_livestream_id(user_id, livestream_id);
