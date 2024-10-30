437245e968d6f239dbc4348ea95634381cca121a
yyamada12 <12yacropolisy@gmail.com>
2024-10-30 02:11:57 +0000

    [20086] add index to tables

--- webapp/sql/index.sql
+++ webapp/sql/index.sql
@@ -1,2 +1,6 @@
 alter table icons add index idx_user_id(user_id);
 alter table livestream_tags add index idx_livestream_id(livestream_id);
+alter table livecomments add index idx_livestream_id(livestream_id);
+alter table livestreams add index idx_user_id(user_id);
+alter table reactions add index idx_livestream_id(livestream_id);
+alter table themes add index idx_user_id(user_id);
